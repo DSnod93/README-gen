d4e9a081196896f0fda2879b8ab6811a11b95850
DSnoddy <douglassnoddyjr@gmail.com>
2021-04-04 17:16:02 -0800

    added questions to prompt user

--- index.js
+++ index.js
@@ -1,7 +1,60 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Enter your Github username',
+            name: 'username'
+        },
+        {
+            type: 'input',
+            message: 'Enter your email',
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: 'Enter title of project',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Enter description of the project',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'Enter installation instructions',
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: 'Enter usage instructions',
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: 'Enter collaborators if any',
+            name: 'credits'
+        },
+        {
+            type: 'input',
+            message: 'Enter badges if any',
+            name: 'badges'
+        },
+        {
+            type: 'input',
+            message: 'Enter features of the project',
+            name: 'features'
+        },
+    ])
+};
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -10,4 +63,4 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
-init();
+promptUser();
